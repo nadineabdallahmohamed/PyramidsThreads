348642f32c4527a0c690e715441f7aa780734293
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-11 22:45:00 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package threadsPyramids;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		PyramidDAO pr=new PyramidDAO();
+		pr.start();
+		
+		
+		//System.out.print(pr.p_list);
+		
+		
+
+	}
+
+}
